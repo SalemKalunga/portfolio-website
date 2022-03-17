12b57887ed8711d3999213a65154be35b1522566
Salem Koni <salemlegrand65@gmail.com>
2022-03-17 22:40:50 +0100

    nav bar is responsive

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".bars_container");
+const links = document.querySelector(".links_container");
+
+btn.addEventListener("click", function() {
+
+    links.classList.toggle("active");
+})
