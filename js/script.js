2c3da58131d096fd95452f2232184ed7b223703e
Salem Koni <salemlegrand65@gmail.com>
2022-03-17 22:50:28 +0100

    modify the wrong written text

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 const btn = document.querySelector(".bars_container");
 const links = document.querySelector(".links_container");
-
 btn.addEventListener("click", function() {
-
     links.classList.toggle("active");
 })
