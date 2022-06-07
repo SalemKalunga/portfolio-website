bd02a57de4b24097c431f3ff0b61fcd5a355fef0
Salem Koni <salemlegrand65@gmail.com>
2022-06-07 20:36:38 +0200

    restart the work from scratch

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector(".bars_container");
-const links = document.querySelector(".links_container");
-btn.addEventListener("click", function() {
-    links.classList.toggle("active");
-})
