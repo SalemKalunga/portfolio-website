3f65c120920e3779735a632f2a1f71995a11c363
salem kalunga <salemlegrand65@gmail.com>
2022-11-26 12:58:03 +0200

    finis la partie services

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 window.onload = function() {
-
+// alert("hello world")
 }
-let ul = document.querySelector(".navbar_ul");
-let clickme = document.querySelector(".bars_container");
+let ul = document.querySelector(".link_container ul");
+let clickme = document.querySelector(".humberger_menu");
 clickme.addEventListener("click", function() {
     ul.classList.toggle("active")
 })
+let lis = document.querySelectorAll(".link_container ul li")
+lis.forEach(element => {
+   element.addEventListener("click",()=>{
+    ul.classList.toggle("active")
+   }) 
+});
